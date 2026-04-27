631525b0c876bd028242e5168fd8a5b5f6a5c9c6
Enzo Antônio Ferreira de Araújo <enzo.a.araujo6@aluno.senai.br>
2026-04-27 14:44:21 -0300

    Aula 05 (Hooks) - 27/04/2026
    Realização de atividades acerca de useState, useRef e useEffect.

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import Exercicio10 from "./flexbox_exercises-EnzoAntonioFerreiraDeAraujo/exercises_10";
+import ContadorExample from "./hooks/examples/useState_example";
+import TelaLogin from "./hooks/examples/useRef_example";
+import TelaMoeda from "./hooks/examples/useEffect_example";
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exercicio10 />
+      <TelaMoeda/>
     </View>
   );
 }
